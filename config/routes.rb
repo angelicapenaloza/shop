58f01d196e6a85c020d585b0915de0c4e9f9ea3e
EC2 Default User <ec2-user@ip-172-31-8-113.us-east-2.compute.internal>
2018-10-01 19:59:20 +0000

    Changed routes from index to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-    root "index#index"
-
-
+    root "home#index"
   resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
