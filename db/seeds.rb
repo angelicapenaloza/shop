3861bd247c8afe2e916248fe0a8685d08b7e27d9
Angelica Penaloza <ampenalozag@unal.edu.co>
2018-10-03 10:28:40 -0700

    pruebaSeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ A=Product.create(nombre: 'white T-shirt', precio: 29.99, description: 'Really cool white t-shirt',
+  image: 'https://ae01.alicdn.com/kf/HTB1ZYrRRVXXXXc8XVXXq6xXFXXXH/EnjoytheSpirit-Cotton-T-shirt-No-Printing-Plain-T-Shirt-T-Shirt-Men-with-No-Logo-Printing.jpg_640x640.jpg')
